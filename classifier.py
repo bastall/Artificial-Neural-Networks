032df47ab16fae86bbc94f7d1cd91129435dab9a
Reitumetse <r.s.sephoko@gmail.com>
2025-04-27 16:48:19 +0200

    Load FashionMNIST dataset with custom transform

--- classifier.py
+++ classifier.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torchvision
+from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
 class FashionClassifier(nn.Module):
@@ -14,3 +15,10 @@ def load_data(batch_size=64):
         transforms.ToTensor(), # Convert images to PyTorch tensors
         transforms.Normalize((0.5,), (0.5,)) # Normalize to range [-1,1]
     ])
+    
+    train_dataset = datasets.FashionMNIST(
+        root = ".",
+        train = True,
+        transform = transform,
+        download = False
+    )
